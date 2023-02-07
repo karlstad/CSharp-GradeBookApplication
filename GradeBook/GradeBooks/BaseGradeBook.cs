892adf16ee3f20f5148b2085eb0c55382cec68ae
Øyvind Karlstad <oeyvind_karlstad@yahoo.no>
2023-02-07 20:31:24 +0100

    Add Support for Weighted Grading

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,16 +9,18 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class BaseGradeBook
+    public abstract class BaseGradeBook
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -107,20 +109,22 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
-            switch (letterGrade)
+            var grade = letterGrade switch
             {
-                case 'A':
-                    return 4;
-                case 'B':
-                    return 3;
-                case 'C':
-                    return 2;
-                case 'D':
-                    return 1;
-                case 'F':
-                    return 0;
+                'A' => 4,
+                'B' => 3,
+                'C' => 2,
+                'D' => 1,
+                'F' => 0,
+                _ => 0
+            };
+
+            if (IsWeighted && (StudentType.Honors.Equals(studentType) || StudentType.DualEnrolled.Equals(studentType)))
+            {
+                return ++grade;
             }
-            return 0;
+
+            return grade;
         }
 
         public virtual void CalculateStatistics()
--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,9 +7,29 @@ namespace GradeBook.GradeBooks;
 
 public class RankedGradeBook : BaseGradeBook
 {
-    public RankedGradeBook(string name) : base(name)
+    public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
     {
         Type = GradeBookType.Ranked;
+    }
+
+    public override void CalculateStatistics()
+    {
+        if (Students.Count < 5)
+        {
+            Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            return;
+        }
+        base.CalculateStatistics();
+    }
+
+    public override void CalculateStudentStatistics(string name)
+    {
+        if (Students.Count < 5)
+        {
+            Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            return;
+        }
+        base.CalculateStudentStatistics(name);
     }
 
     public override char GetLetterGrade(double averageGrade)
